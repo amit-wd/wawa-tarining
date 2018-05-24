49ef5b5a3ac3b767f181faaac5f1280d90e4d7be
Andrew Crites <acrites@mobiquityinc.com>
2018-05-24 15:24:28 -0400

    feat(jest-config): Breaking out and normalizing jest configuration.
    This includes the addition of a transformer for babel that circumvents .babelrc (required by Metro for building) to keep snapshot rendering more consistent across environments

---
+++ jest.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  preset: 'react-native',
+  setupFiles: ['./src/setupJest.js'],
+  transform: {
+    '^.+\\.jsx?$': '<rootDir>/src/transformJest.js',
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|react-navigation)',
+  ],
+  testRegex: '/__tests__/.+\\.test\\.tsx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  collectCoverageFrom: ['**/*.tsx', '**/*.ts'],
+  moduleNameMapper: {
+    'src(.*)': '<rootDir>/src/$1',
+  },
+  globals: {
+    'ts-jest': {
+      tsConfigFile: 'tsconfig.test.json',
+    },
+  },
+};
